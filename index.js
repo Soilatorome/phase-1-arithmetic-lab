aeb6147882bb0d8fc7c57315d749f8f934e3b188
soilatorome <soila.torome.st@gmail.com>
2023-07-13 22:52:42 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// let answer = 62
+// function multiply(num1, num2) {
+//     num1 * num2 
+// }
+// let num1 = 31
+// let num2 = 2
+// function multiply(num1, num2) {
+//     return num1*num2
+// }
+let num1= 31;
+let num2 =2;
+const multiply = num1* num2;
+let integer = Math.floor (Math.random()*10);
+ const random = integer;
+
+ let num3 = 11
+ let num4 = 7
+ const mod = num3 % num4;
+
+ const max = Math.max(20,7,4,5);
